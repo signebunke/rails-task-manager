7b0450de49c9d18b3059fbefc4cf6d5031063a05
Signe Bunke <signebunke@gmail.com>
2021-01-26 19:40:58 +0100

    MVC setup

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,18 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # CRUD
+  # CREATE, READ, UPDATE, DELETE
+  # read all the tasks
+  # get '/tasks', to: 'tasks#index'
+  # # read one task
+  # get '/tasks/:id', to: 'tasks#show'
+  # # create a task
+  # get '/tasks/new', to: 'tasks#new' # display the form
+  # post '/tasks', to: 'tasks#create'
+  # # update a task
+  # get '/tasks/:id/edit', to: 'tasks#edit'
+  # patch '/tasks/:id', to: 'tasks#update'
+  # # delete a task
+  # delete '/tasks/:id', to: 'tasks#destroy'
+  resources :tasks
 end
