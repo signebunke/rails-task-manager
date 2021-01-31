5a70116c19d11cd84a9718c6e870d07a4920dbe6
Signe Bunke <signebunke@gmail.com>
2021-01-31 13:29:46 +0100

    app updated

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,11 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  # CRUD
-  # CREATE, READ, UPDATE, DELETE
-  # read all the tasks
-  # get '/tasks', to: 'tasks#index'
-  # # read one task
-  # get '/tasks/:id', to: 'tasks#show'
-  # # create a task
-  # get '/tasks/new', to: 'tasks#new' # display the form
-  # post '/tasks', to: 'tasks#create'
-  # # update a task
-  # get '/tasks/:id/edit', to: 'tasks#edit'
-  # patch '/tasks/:id', to: 'tasks#update'
-  # # delete a task
-  # delete '/tasks/:id', to: 'tasks#destroy'
+  # get 'tasks', to: 'tasks#index'
+  # post 'tasks', to: 'tasks#create'
+  # get 'tasks/new', to: 'tasks#new', as: :new_task
+  # get 'tasks/:id', to: 'tasks#show', as: :task
+  # get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  # patch 'tasks/:id', to: 'tasks#update'
+  # delete 'tasks/:id', to: 'tasks#destroy'
+
   resources :tasks
 end
